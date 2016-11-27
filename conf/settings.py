93ba3887ae5366989ba428b23208f2b60bda29cb
Paul Boes <paulboes@Pauls-MBP.fritz.box>
2016-11-27 20:51:34 +0100

    changed context porcessor in settings

--- conf/settings.py
+++ conf/settings.py
@@ -48,7 +48,7 @@ TEMPLATES = [{'BACKEND': 'django.template.backends.django.DjangoTemplates',
   'OPTIONS': {'context_processors': ['django.template.context_processors.debug',
                                      'django.template.context_processors.request',
                                      'django.contrib.auth.context_processors.auth',
-                                     'graphite.context_processors.site',
+                                     'conf.context_processors.site',
                                      'django.contrib.messages.context_processors.messages']}}]
 WSGI_APPLICATION = 'conf.wsgi.application'
 
