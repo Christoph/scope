5825341715451ecfc68fe50d5f7aefaaf1e8e76a
Chris <christoph.kralj@gmail.com>
2016-12-01 11:46:29 +0100

    Updated Article model and migrations

--- scope/models.py
+++ scope/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django import forms
 from django.contrib.auth.models import User
 # Create your models here.
-    
+
 # Create your models here.
 class UserProfile(models.Model):
     user = models.ForeignKey(User, default = None,null = True)
@@ -15,10 +15,11 @@ class Article(models.Model): #formerly Nodes
     url = models.CharField(max_length=200)
     summary = models.CharField(max_length=200)
     keywords = models.CharField(max_length=200)
+    text = models.TextField()
     def __unicode__(self):              # __unicode__ on Python 2
         return self.title
 
-# class Customer(models.Model):     
+# class Customer(models.Model):
 #     title = models.CharField(max_length=200)
 #     id = models.IntegerField()
 #     def __unicode__(self):              # __unicode__ on Python 2
