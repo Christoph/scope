f6f119fb18bb296baba68e08c8e6c9a7342afbff
Chris <christoph.kralj@gmail.com>
2016-12-02 18:34:20 +0100

    Updating

--- curate/customers/create_newsletter_nh.py
+++ curate/customers/create_newsletter_nh.py
@@ -262,215 +262,26 @@ doc = [d["doc"] for d in data]
 
 term_vecs, docs = pre.lemma([d for d in doc])
 
-punc = re.compile('[%s]' % re.escape(string.punctuation))
-term_vec = []
-
-for a in doc:
-    a = a.lower()  # these aren't necessary if you're dealing with keywords
-    a = punc.sub('', a)
-    term_vec.append(nltk.word_tokenize(a))
-
-
-# Print resulting term vectors
-
-# Remove stop words from term vectors
-
-stop_words = nltk.corpus.stopwords.words('english')
-
-for i in range(0, len(term_vec)):
-    term_list = []
-
-    for term in term_vec[i]:
-        if term not in stop_words:
-            term_list.append(term)
-
-    term_vec[i] = term_list
-
-# Print term vectors with stop words removed
-# stemming is difficult with the keyword extraction ....
-    # Porter stem remaining terms
-
-porter = nltk.stem.porter.PorterStemmer()
-
-for i in range(0, len(term_vec)):
-    for j in range(0, len(term_vec[i])):
-        term_vec[i][j] = porter.stem(term_vec[i][j])
-
-#  Convert term vectors into gensim dictionary
-
-dict = gensim.corpora.Dictionary(term_vec)
-corp = []
-for i in range(0, len(term_vec)):
-    corp.append(dict.doc2bow(term_vec[i]))
-
-#  Create TFIDF vectors based on term vectors bag-of-word corpora
-
-tfidf_model = gensim.models.TfidfModel(corp)
-
-#  Create pairwise document similarity index
-
-
-corpus_tfidf = tfidf_model[corp]
-# lsi_model = gensim.models.LsiModel(corpus_tfidf, id2word=dict, num_topics=n1) #
-# corpus_lsi = lsi_model[corpus_tfidf]
-# list_corpus = []
-# # for dox in corpus_lsi:
-# #     list_corpus.append(dox)
-# index = gensim.similarities.SparseMatrixSimilarity(corpus_lsi, num_features = n1 )
-
-# lda_model = gensim.models.LdaModel(corpus_tfidf, id2word=dict, num_topics=20) #initialize an LSI transformation
-#index2 = gensim.similarities.SparseMatrixSimilarity(lda_model[corpus_tfidf], num_features = 50 )
-
-# /home/django/graphite/static/last24h/l24h.mm', corp)
-# gensim.corpora.MmCorpus.serialize(
-#    settings.STATIC_ROOT + 'rene/rene_data/rene.mm', corp)
-# dict.save(settings.STATIC_ROOT + 'rene/rene_data/rene.dict')
-# lsi_model.save(settings.STATIC_ROOT + 'rene/rene_data/rene.lsi')
-# index.save(settings.STATIC_ROOT + 'rene/rene_data/rene.index')
-# lda_model.save('/tmp/model.lda')
-
-
 # Begin Graph visualisation
-
-# 3-articlenumber*0.03/500#0.1/pow(upper/210,2)  #the higher the thresh,
-# the more critical
 
 ug = nx.Graph()
 
-for i in range(0, len(corp)):
+for i in range(0, len(data)):
     try:
         source = urlparse(urls[i]).hostname
     except:
         source = "No Source Information"
-
-    # if len(urls[i].split("www.")) != 1:
-    #     source = urls[i].split("www.")[1].split("/")[0]
-    # elif len(urls[i].split("rss.")) != 1:
-    #     source = urls[i].split("rss.")[1].split("/")[0]
-    # else:
-    #     source = urls[i].split("http://")[1].split("/")[0]
-    # ug.add_node(i, title=titles[i], url=urls[i], suggest=0, summary=summary[i],
-    #             images=images[i], comp=0, source=source, keywords='',
-    #             time=None)
 
     ug.add_node(i, title=data[i]["titles"], url=data[i]["urls"], suggest=0, summary=data[i]["doc"][0:200],
                 images=data[i]["images"], comp=0, source=source, keywords='',
                 time=None)
 
 ug_nl = json_graph.node_link_data(ug)
-#tgt_mobile = json_graph.tree_data(tg2,root=0)
-# with open('curate/data/rene_all.json', 'w+') as fp:
-#    json.dump(ug_nl, fp)
-
 
 graphs = []
 score_new = 0
 best_thresh = 0.
 best_score = 0
-#thresh = 0.02
-
-# Now trying two steps: In the first, get the bigguest cluster (which
-# should correspond to tech topics) and then in a second step maximise the
-# number of clusters in it
-
-orignumber = len(ug)
-print "First Round"
-print str(len(ug)) + " old graph"
-
-# for s in [x/1000. for x in xrange(0,500)]:
-
-
-# #while score_new >= score_old:#len(graphs) not in [5,6] and any(len(x) <4 for x in graphs):
-#     ug.remove_edges_from(ug.edges())
-
-#     for i in range( 0, len( corpus_tfidf ) ):
-#         sim = index[ lsi_model[ corp [ i ] ] ]
-#         for j in range( i+1, len( sim ) ):
-#             dist = (1. - sim[j])/2.
-#             if dist < s:
-#                 ug.add_edge(i,j,{'weight':dist})
-#     graphs = sorted(nx.connected_component_subgraphs(ug),key=len,reverse=True)
-
-#     test = [x for x in graphs if len(x) >= 3]#20>=
-#     exclude = [x.nodes() for x in graphs if x not in test]
-#     test2 = [len(x) for x in test]
-#     if len(test) == 2:
-#         score_new = len(test[0])*len(test[1])#reduce(mul, test2,1)#len(test[0])+len(test[1])
-#     #score_new = [len(test),sum(test2)]
-#     elif len(test) >= 3:
-#         score_new = len(test[0])*len(test[1])*len(test[2])
-
-#     if score_new > best_score and len(test) >= 3:
-#         best_score = score_new
-#         best_thresh = s
-
-# print thresh
-#thresh += 0.001
-# print s
-# print len(graphs)
-# # for i in graphs:
-# #     for ii in i:
-# #         print ug.node[ii]['title']
-# #     print "and"
-# #print test2
-# print score_new
-# print best_thresh
-# if thresh >= 0.5:
-#     break
-
-
-#thresh -= 0.002
-# print best_thresh
-# ug.remove_edges_from(ug.edges())
-# for i in range( 0, len( corpus_tfidf ) ):
-#     sim = index[ lsi_model[ corp [ i ] ] ]
-#     for j in range( i+1, len( sim ) ):
-#         dist = (1. - sim[j])/2.
-#         if dist < best_thresh:
-#             ug.add_edge(i,j,{'weight':dist})
-# graphs = sorted(nx.connected_component_subgraphs(ug),key=len,reverse=True)
-# test = [x for x in graphs if len(x) >= 3]#20>=
-# exclude = [x.nodes() for x in graphs if x not in test]
-# for graph in graphs:
-#     print graph
-#     for no in graph:
-#         print ug.node[no]['title']
-
-
-# for graph in test:
-#     print "\n CLUSTER: \n"
-#     for no in graph:
-#         try:
-#             print ug.node[no]['title'], ug.node[no]['source'], no
-#         except:
-#             pass
-# print "\n AND \n"
-# for no in list(set().union(*exclude)):
-#     try:
-#         print ug.node[no]['title'], ug.node[no]['source'], no
-#     except:
-#         pass
-
-# print "\n SECOND ROUND \n"
-
-# for i in range(1,len(graphs)):
-#     ug.remove_nodes_from(graphs[i].nodes())
-
-# print str(len(ug)) + " new graph"
-
-lsi_model2 = gensim.models.LsiModel(corpus_tfidf, id2word=dict, num_topics=n2)
-corpus_lsi2 = lsi_model2[corpus_tfidf]
-# list_corpus = []
-# for dox in corpus_lsi:
-#     list_corpus.append(dox)
-index2 = gensim.similarities.SparseMatrixSimilarity(
-    corpus_lsi2, num_features=n2)
-
-# lsi_model2.save(settings.STATIC_ROOT + 'rene/rene_data/l24h2.lsi')
-# index2.save(settings.STATIC_ROOT + 'rene/rene_data/l24h2.index')
-
-best_thresh = 0.
-best_score = 0  # [0,0]
 
 # Optimization
 for s in [x / 10000. for x in xrange(1, 201)]:
@@ -481,10 +292,9 @@ for s in [x / 10000. for x in xrange(1, 201)]:
     # Remove old edges
     ug.remove_edges_from(ug.edges())
 
-    for i in range(0, len(corpus_tfidf)):
+    for i in range(0, len(data)):
         # TODO: SIMILARITY
         sim = wv_model.similarity(docs, i)
-        # sim = index2[lsi_model2[corp[i]]]
         for j in range(i + 1, len(sim)):
             dist = (1. - sim[j])
             if dist < s and j in ug and i in ug:
@@ -494,11 +304,11 @@ for s in [x / 10000. for x in xrange(1, 201)]:
 
     test = [x for x in graphs if 20 > len(x) >= 3]
     exclude = [x.nodes() for x in graphs if x not in test]
-    #test2 = [len(x) for x in test]
+    # test2 = [len(x) for x in test]
     if len(test) >= 2:
         # len(test[0])+len(test[1])#sum(test2)#len(test)#test2[0]
         score_new = len(test)
-        #score_new = [len(test),sum(test2)]
+        # score_new = [len(test),sum(test2)]
 
     # score_new = sum(test2) #[len(test),sum(test2)]
 
@@ -507,7 +317,7 @@ for s in [x / 10000. for x in xrange(1, 201)]:
         best_thresh = s
 
         # print thresh
-        #thresh += 0.001
+        # thresh += 0.001
         print s
         print len(graphs)
         # for i in graphs:
@@ -524,7 +334,7 @@ for s in [x / 10000. for x in xrange(1, 201)]:
 dispersion = str((1. - 2 * best_thresh) * 100)[:-2] + '%'
 print best_thresh
 ug.remove_edges_from(ug.edges())
-for i in range(0, len(corpus_tfidf)):
+for i in range(0, len(data)):
     sim = wv_model.similarity(docs, i)
     # sim = index2[lsi_model2[corp[i]]]
     for j in range(i + 1, len(sim)):
@@ -579,9 +389,10 @@ for a in graphx:
         for i in comp:
             ug.node[i]['comp'] = count_comp
             ug.node[i]['single'] = 0
-            for word in punc.sub('', ug.node[i]['title']).split(" "):
+            for word in preprocess.punc.sub(
+                    '', ug.node[i]['title']).split(" "):
                 # print word
-                if word not in stop_words:
+                if word not in preprocess.stop_words:
                     all_words.append(word)
         a = sorted([[len([b for b in all_words if b == word]), word]
                     for word in list(set(all_words))], reverse=True)
