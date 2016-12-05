1ff000ac1a8e5ede54b7263493de3b70e8a4fb44
Chris <christoph.kralj@gmail.com>
2016-12-05 17:18:15 +0100

    Added new models to admin page

--- scope/admin.py
+++ scope/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Customer, Article
+from .models import Customer, Article, Source, AgentImap
 
 admin.site.register(Customer)
 admin.site.register(Article)
+admin.site.register(Source)
+admin.site.register(AgentImap)
