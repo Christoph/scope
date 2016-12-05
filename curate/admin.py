20a991e9544aa54b4b4c889104e18aeb67adece6
Chris <christoph.kralj@gmail.com>
2016-12-05 11:45:38 +0100

    Added moels to the admin view

--- curate/admin.py
+++ curate/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Curate_Customer, Curate_Query, Article_Curate_Query
+
+admin.site.register(Curate_Customer)
+admin.site.register(Curate_Query)
+admin.site.register(Article_Curate_Query)
