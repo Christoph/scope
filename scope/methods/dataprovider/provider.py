39b4766b475ca919f3107e75f963fd978c2dfded
Chris <christoph.kralj@gmail.com>
2016-12-11 11:12:48 +0100

    Debugging and merginf pauls changes

--- scope/methods/dataprovider/provider.py
+++ scope/methods/dataprovider/provider.py
@@ -1,13 +1,15 @@
 
-from . import ImapHandler
+from . import imap_handler
 from scope.models import AgentImap
+
+reload(imap_handler)
 
 
 class Provider(object):
     """docstring for crawler."""
 
     def __init__(self):
-        self.imap = ImapHandler()
+        self.imap = imap_handler.ImapHandler()
 
     def query_agent(self, agent):
         if type(agent) == AgentImap:
