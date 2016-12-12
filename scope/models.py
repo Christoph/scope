55b4185ebc43d7bba45f9e259b06c9d0af8432ac
Paul Boes <paulboes@Pauls-MBP.fritz.box>
2016-12-12 22:42:24 +0100

    increased value of image field for articles because that keeps making problems

--- scope/models.py
+++ scope/models.py
@@ -54,7 +54,7 @@ class Article(models.Model):
     source = models.ForeignKey(Source, blank=True, null=True)
     title = models.CharField(max_length=300)
     url = models.CharField(max_length=500)
-    images = models.CharField(max_length=500, blank=True)
+    images = models.CharField(max_length=1000, blank=True)
     description = models.CharField(max_length=500, blank=True)
     keywords = models.CharField(max_length=200, blank=True)
     body = models.TextField()
