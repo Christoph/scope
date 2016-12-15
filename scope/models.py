89ff7e114a7a1524d6cd80c9ef65ed45dcdc0e90
Paul Boes <paulboes@wlan254-94.extern.hu-berlin.de>
2016-12-15 13:24:22 +0100

    Modified Article field max length

--- scope/models.py
+++ scope/models.py
@@ -52,8 +52,8 @@ class AgentImap(models.Model):
 
 class Article(models.Model):
     source = models.ForeignKey(Source, blank=True, null=True)
-    title = models.CharField(max_length=300)
-    url = models.CharField(max_length=500)
+    title = models.CharField(max_length=500)
+    url = models.CharField(max_length=1000)
     images = models.CharField(max_length=1000, blank=True)
     description = models.CharField(max_length=500, blank=True)
     keywords = models.CharField(max_length=200, blank=True)
