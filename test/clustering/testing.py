51009d933bd27ae48a4d1182d536fda9e4c81d5e
Chris <christoph.kralj@gmail.com>
2016-12-15 18:59:35 +0100

    using a global variable to use django objects in the test files

--- test/clustering/testing.py
+++ test/clustering/testing.py
@@ -1,12 +1,22 @@
-from scipy.cluster.hierarchy import dendrogram, linkage
+import django
+django.setup()
+
 import numpy as np
-from scope.methods.graphs import cluster_plot as plt
+import json
+from django.core import serializers
+
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.metrics import pairwise_distances
 
+from scipy.cluster.hierarchy import dendrogram, linkage
 from scipy.cluster.hierarchy import cophenet
 from scipy.spatial.distance import pdist
 from scipy.cluster.hierarchy import fcluster
+
+from scope.models import Article
+import scope.methods.semantics.word_vector as word_vector
+
+wv = word_vector.Model("en")
 
 # Generate test data
 np.random.seed(4711)  # for repeatability of this tutorial
@@ -19,6 +29,18 @@ d = np.random.multivariate_normal([80, 80], [[30, 1], [1, 30]], size=[200,])
 e = np.random.multivariate_normal([0, 100], [[100, 1], [1, 100]], size=[200,])
 X2 = np.concatenate((X, c, d, e),)
 
+# Get data from json
+with open('test/clustering/articles.json', 'r') as stream:
+    data = json.load(stream)
+
+articles = []
+for obj in serializers.deserialize("json", data):
+    articles.append(obj.object)
+
+# Get semantic informations
+wv.load_data(articles)
+vec = wv.document_vectors()
+sim = wv.similarity_matrix()
 
 # clustering
 
