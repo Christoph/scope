4cc76af2e9e93b7db6d725dbc6cb3d092bc0a520
Chris <christoph.kralj@gmail.com>
2016-12-21 13:24:34 +0100

    Added bayes clustering

--- test/clustering/compare_clusterings.py
+++ test/clustering/compare_clusterings.py
@@ -32,5 +32,7 @@ def get_labels(sim, vecs):
     links_hc, labels_hc = clustering_methods.hierarchical_clustering(vecs, "ward", "euclidean", "maxclust", 12)
     labels_dbscan = clustering_methods.db_scan(sim, threshold=0.3, metric="euclidean", algorithm="auto")
     labels_km, centers_km = clustering_methods.k_means(vecs, 16)
+    labels_gmm = clustering_methods.gauss_mix(vecs,16)
+    labels_bgmm = clustering_methods.bayes_gauss_mix(vecs,16)
 
-    return {"l_sim": labels_sim, "l_aff": labels_aff, "l_hc":labels_hc, "l_db": labels_dbscan, "l_km": labels_km}
+    return {"l_sim": labels_sim, "l_aff": labels_aff, "l_hc":labels_hc, "l_db": labels_dbscan, "l_km": labels_km, "l_gmm": labels_gmm, "l_bgmm":labels_bgmm}
