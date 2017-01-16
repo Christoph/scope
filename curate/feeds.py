fed0df65030707051e6616ca2601d4f159c946e7
Paul Boes <paulboes@Pauls-MacBook-Pro.local>
2017-01-16 16:59:17 +0100

    MOdified feed so that it produces the tags required by MailChimp

--- curate/feeds.py
+++ curate/feeds.py
@@ -1,13 +1,11 @@
 from django.contrib.syndication.views import Feed
-from django.urls import reverse
+from django.utils.feedgenerator import Rss201rev2Feed
 
 #from .models import Select
-
-from datetime import date, datetime
 import string
 
 from curate.models import Curate_Query, Article_Curate_Query, Curate_Customer, Curate_Customer_Selection
-from scope.models import Customer, UserProfile
+from scope.models import Customer
 
 def truncate_words_and_prod_sentence(s, thresh):
     split = s.split(' ')
@@ -21,8 +19,36 @@ def truncate_words_and_prod_sentence(s, thresh):
     final = string.join(final.split('.')[:-1], ".") + '.'
     return final
 
+# class ExtendedRSSFeed(Rss201rev2Feed):
+class ExtendedRSSFeed(Rss201rev2Feed):
+    """
+    Create a type of RSS feed that has content:encoded elements.
+    """
+    def root_attributes(self):
+        attrs = super(ExtendedRSSFeed, self).root_attributes()
+        # Because I'm adding a <content:encoded> field, I first need to declare
+        # the content namespace. For more information on how this works, check
+        # out: http://validator.w3.org/feed/docs/howto/declare_namespaces.html
+        attrs['xmlns:media'] = 'http://search.yahoo.com/mrss/'
+        return attrs
+    
+    def add_item_elements(self, handler, item):
+        super(ExtendedRSSFeed, self).add_item_elements(handler, item)
+        if 'media:content' in item:
+            handler.addQuickElement(u"media:content", item['media:content'])
+        if 'source' in item:
+            handler.addQuickElement(u"source", item['source'])
+
+        # 'content_encoded' is added to the item below, in item_extra_kwargs()
+        # It's populated in item_your_custom_field(). Here we're creating
+        # the <content:encoded> element and adding it to our feed xml
+        # if item['media_content'] is not None:
+        #     handler.addQuickElement(u'media_content', item['media_content'])
+
+
 
 class Feed(Feed):
+    feed_type = ExtendedRSSFeed
     title = "Today's NHBrief"
     link = "/nh/"
     description = "Newsletter created with the help of Scope Technology"
@@ -56,6 +82,28 @@ class Feed(Feed):
         #     i in .filter(selection_options__in = select_option).order_by("rank")
         # suggestions = [i.article for i in article_query_instances]
         return suggestions
+    
+    def item_extra_kwargs(self, item):
+        # This is probably the first place you'll add a reference to the new
+        # content. Start by superclassing the method, then append your
+        # extra field and call the method you'll use to populate it.
+        extra = super(Feed, self).item_extra_kwargs(item)
+        extra.update({'media:content': self.item_your_custom_field(item)})
+        extra.update({'source': self.item_source_custom_field(item)})
+        return extra
+
+    def item_your_custom_field(self, item):
+        # This is your custom method for populating the field.
+        # Name it whatever you want, so long as it matches what
+        # you're calling from item_extra_kwargs().
+        # What you do here is entirely dependent on what your
+        # system looks like. I'm using a simple queryset example,
+        # but this is not to be taken literally.
+        # full_text = query_obj['full_story_content']
+        return item.images
+
+    def item_source_custom_field(self, item):
+        return item.source.name
 
     def item_title(self, item):
         return item.title
@@ -71,10 +119,13 @@ class Feed(Feed):
         # return datetime.strftime(item.pubdate.replace(tzinfo=None),'%a %x %H:%M')
         return item.pubdate
 
-    def item_enclosure_url(self, item):
-        return item.images
+    # def item_enclosure_url(self, item):
+    #     return item.images
 
-    item_enclosure_mime_type = "jpeg"
+    # def item_content_encoded(self, item):
+    #     return "test"
 
-    def item_author_name(self, item):
-        return item.source.name
+    # item_enclosure_mime_type = "image/jpeg"
+
+    # def item_source(self, item):
+    #     return item.source.name
