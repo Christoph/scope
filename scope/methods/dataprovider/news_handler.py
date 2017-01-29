9181e8c7108a427e47e66b4776d631543f180d84
Chris <christoph.kralj@gmail.com>
2017-01-29 11:12:46 +0100

    Created newspaper handler

---
+++ scope/methods/dataprovider/news_handler.py
@@ -0,0 +1,73 @@
+'''
+Handels newspaper crawling
+'''
+
+import newspaper
+
+from scope.models import Article
+
+
+class NewsSourceHandler(object):
+    """NewsPaperHandler."""
+
+    def _download_articles(self, articles):
+        for a in articles:
+            try:
+                a.download()
+                a.parse()
+
+                # Remove newline characters
+                a.text = a.text.replace("\n", "")
+            except:
+                print "Error while downloading: " + a.url
+                continue
+
+        print "Articles downloaded and parsed"
+        return articles
+
+    def _check_urls(self, articles):
+        out = []
+
+        for a in articles:
+            if Article.objects.filter(url=a.url).exists():
+                print "Url already exists: " + a.url
+            else:
+                out.append(a)
+
+        return out
+
+    def get_articles_from_list(self, url_list, language):
+        ''' Download and parse articles from list.'''
+        articles = []
+
+        newspaper_lang_dict = {
+            'ger': 'de',
+            'eng': 'en',
+        }
+
+        # Create newspaper article list
+        for i in range(0, len(url_list)):
+            try:
+                if language == "mix":
+                    articles.append(newspaper.Article(url_list[i]))
+                else:
+                    articles.append(newspaper.Article(
+                        url_list[i],
+                        language=newspaper_lang_dict[language]))
+            except:
+                print "Error while  converting " + url_list[i]
+                continue
+
+        # out = self._download_articles(self._check_urls(articles))
+        out = self._download_articles(articles)
+
+        return out
+
+    def get_articles_from_source(self, url):
+        ''' Download and parse articles from source.'''
+        source = newspaper.build(url, memoize_articles=False)
+
+        # articles = self._check_urls(source.articles)
+        articles = source.articles
+
+        return self._download_articles(articles)
