69d21eb4e69cf044f912b85c573d820731601628
Paul Boes <paulboes@Pauls-MBP.fritz.box>
2017-02-03 10:13:41 +0100

    changed celery config

--- scope/celery.py
+++ scope/celery.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 import os
 from celery import Celery
+from conf.settings import base
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings.base')
@@ -13,7 +14,7 @@ app = Celery('scope')#,broker='amqp://localhost')#http://localhost:15672')#amqp:
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
 app.config_from_object('django.conf:settings')
-app.autodiscover_tasks()#lambda: base.INSTALLED_APPS
+app.autodiscover_tasks(lambda: base.INSTALLED_APPS)#
 
 # app.conf.update(
 # 	# CELERY_RESULT_BACKEND = 'cache',
