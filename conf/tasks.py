c691ce86c534673036a4c0f60b7ee811ee5a8380
Paul Boes <paulboes@Pauls-MBP.fritz.box>
2017-02-09 00:56:48 +0100

    had to take content from conf.tasks so that supervisor loads

--- conf/tasks.py
+++ conf/tasks.py
@@ -1,84 +1,12 @@
 from __future__ import absolute_import
 
 from celery import shared_task, current_task
-from celery.exceptions import Reject
-from time import sleep, mktime
-
-import sys
-from django.templatetags.static import static
-
-import Queue
-import threading
-import time
-import networkx as nx
-import feedparser
-import newspaper
-from newspaper import Article
-import untangle
-import sys
-import json
-import urllib
-import re
-import math
-import gensim
-import nltk
-import string
-from datetime import timedelta
-from datetime import datetime
-import copy
 from django.conf import settings
-#import logging
-from networkx.readwrite import json_graph
-from django.core.mail import send_mail
-
-global workQueue, exitFlag, queueLock, current_name, articlenumber, inputt
-
-class myThread (threading.Thread):
-	def __init__(self, threadID, name, q):
-		threading.Thread.__init__(self)
-		self.threadID = threadID
-		self.name = name
-		self.q = q
-		#self.counter = counter
-	def run(self):
-		print "Starting " + self.name
-		process_data(self.name, self.q)
-		
-		print "Exiting " + self.name
-
-
-
-def process_data(threadName, q):
-	while not exitFlag:
-		queueLock.acquire()
-		if not workQueue.empty():
-			data = q.get()
-			queueLock.release()
-			data.download()
-			data.parse()
-			print " %s processing" % (threadName)
-		else:
-			queueLock.release()
-
-			print " %s release" % (threadName)
-		time.sleep(1)
-
-
-exitFlag = 0
-
+from django.templatetags.static import static
 
 @shared_task
 def test_task(email):
 	print email 
-
-@shared_task
-def twitter_job(twitt_input):
-	#from celery import shared_task, current_task
-	#import json
-	inputdict = {"inputt": twitt_input}
-	print inputdict
-	#sys.argv = [settings.STATIC_BREV + static('last24h/tweet.py'), inputt]
-	execfile(settings.STATIC_ROOT + 'last24h/tweet.py',inputdict)
 
 @shared_task
 def brief_rene(email,strin,select):
