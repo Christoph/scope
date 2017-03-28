602958f1fc97b4ec645d2fd4fd462c7b2331af26
Chris <christoph.kralj@gmail.com>
2017-03-28 14:45:37 +0200

    Added second agent and user

--- research/populate_db_nh.py
+++ research/populate_db_nh.py
@@ -30,3 +30,27 @@ agent = Agent(
 )
 
 agent.save()
+
+
+# Create IMAP object
+imap = AgentImap.objects.get_or_create(user="renesnewsletter", pwd="renewilllesen",
+    imap="imap.gmail.com", mailbox="[Gmail]/All Mail")[0]
+
+# Create Customer
+customer = Customer.objects.get_or_create(
+    name="Neuland Herzer Test", customer_key="nh", email="test@test.com")[0]
+
+# Create Curate_Customer
+curate_customer = Curate_Customer.objects.get_or_create(
+    customer=customer,
+    key="key",
+    defaults = {"expires": datetime.date.today() + datetime.timedelta(days=365)}
+    )[0]
+
+# Create Source for the Curate_Customer
+agent = Agent(
+    product_customer_object=curate_customer,
+    agent_object=imap
+)
+
+agent.save()
