b898d9066ded438460a414c30964e30a7b3e60e5
Chris <christoph.kralj@gmail.com>
2017-04-11 14:52:32 +0200

    Removed old method and finished best one

--- scope/methods/semantics/embedding.py
+++ scope/methods/semantics/embedding.py
@@ -50,7 +50,7 @@ def HAL(docs, window_size=5):
                     data.append(value)
 
             # looking forward
-            for i in range(ind+1, ind+window_size+1):
+            for i in range(ind + 1, ind + window_size + 1):
                 if i < len(toks):
                     term = toks[i]
                     value = abs(i - window_size - ind - 1)
@@ -62,7 +62,8 @@ def HAL(docs, window_size=5):
                     data.append(value)
 
     # Create sparse matrices
-    csr_forward = csr_matrix((data, (row, col)), shape=(len(vocabulary), len(vocabulary)), dtype=float)
+    csr_forward = csr_matrix((data, (row, col)), shape=(
+        len(vocabulary), len(vocabulary)), dtype=float)
     csr_backward = csr_forward.transpose()
 
     # Stack backward and forward matrices
@@ -72,79 +73,7 @@ def HAL(docs, window_size=5):
     hal = hal.multiply(0.5)
 
     # Normalize to [0,1]
-    max_value = 1/hal.max()
-    hal = hal.multiply(max_value)
-
-    return hal, vocabulary
-
-
-def HAL_context(docs, contexts, window_size=5):
-    '''
-        Creating contextual HAL space out of the text documents.
-
-        docs: document list
-        contexts: Consider only docs containing words from the contexts list
-        window_size: window size
-
-        returns: HAL embedding
-    '''
-
-    col = []
-    data = []
-    row = []
-
-    vocabulary = {}
-
-    for text in docs:
-        blob = TextBlob(text)
-
-        # Tokenize the text
-        toks = blob.words
-
-        # Get indicies for all words in the contexts list
-        indicies = [i for i, j in enumerate(toks) if j in contexts]
-
-        # Get surroundings of all context words
-        for ind in indicies:
-            # Add focus word to vocab
-            row_index = vocabulary.setdefault(toks[ind], len(vocabulary))
-
-            # looking backwards
-            for i in range(-window_size + ind, ind):
-                if i >= 0:
-                    term = toks[i]
-                    value = i - ind + window_size + 1
-
-                    index = vocabulary.setdefault(term, len(vocabulary))
-
-                    col.append(row_index)
-                    row.append(index)
-                    data.append(value)
-
-            # looking forward
-            for i in range(ind+1, ind+window_size+1):
-                if i < len(toks):
-                    term = toks[i]
-                    value = abs(i - window_size - ind - 1)
-
-                    index = vocabulary.setdefault(term, len(vocabulary))
-
-                    row.append(row_index)
-                    col.append(index)
-                    data.append(value)
-
-    # Create sparse matrices
-    csr_forward = csr_matrix((data, (row, col)), shape=(len(vocabulary), len(vocabulary)), dtype=float)
-    csr_backward = csr_forward.transpose()
-
-    # Stack backward and forward matrices
-    hal = hstack((csr_backward, csr_forward))
-
-    # Divide by 2 to normalize stacking after stacking
-    hal = hal.multiply(0.5)
-
-    # Normalize to [0,1]
-    max_value = 1/hal.max()
+    max_value = 1 / hal.max()
     hal = hal.multiply(max_value)
 
     return hal, vocabulary
@@ -171,30 +100,38 @@ def HAL_context_grammar(texts, contexts, nlp, window_size=5):
 
     docs = [nlp(a) for a in texts]
 
+    used_deps = ["acomp", "ccomp", "pcomp", "xcomp", "relcl", "conj"]
+    used_tags = ["NN", "NNS", "NNP", "NNPS"]
+    used_ent_deps = ["compound", "dobj", "pobj"]
+
     for doc in docs:
-        temp = []
-
-        # Get important words around the contexts
-        for t in doc:
-            if t.text.lower() in contexts:
-                temp.extend([tok.text for tok in doc if tok.tag_.find("NN") >= 0])
-                # temp.extend([tok.lemma_ for tok in t.subtree if tok.tag_.find("NN") >= 0])
-                temp.extend([tok.text for tok in t.subtree if tok.dep_ in ["acomp", "ccomp", "pcomp", "xcomp", "csubj", "csubjpass", "dobj", "nsubj", "nsubjpass", "pobj"]])
-
-                # temp.extend([tok.lemma_ for tok in t.ancestors if tok.tag_.find("NN") >= 0 or tok.dep_.find("comp") >= 0])
-                temp.extend([tok.text for tok in t.ancestors if tok.dep_ in ["acomp", "ccomp", "pcomp", "xcomp", "csubj", "csubjpass", "dobj", "nsubj", "nsubjpass", "pobj"]])
-
         toks = []
 
-        # Keep word order
-        for t in doc:
-            if t.text in temp and not t.tag_ == "SP":
-                toks.append(t.lemma_)
+        # Prepare entities
+        ents = []
+
+        for e in doc.ents:
+            temp = [t.lemma_ for t in e if t.dep_ in used_ent_deps]
+
+            if len(temp) > 1:
+                ents.append(" ".join(temp))
+
+        # Get important words around in sentecnes containing context words
+        for sent in doc.sents:
+            # Check if a context word is within a sentence
+            if any(sent.text.lower().find(x) >= 0 for x in contexts):
+                for tok in sent:
+                    if tok.tag_ in used_tags or tok.dep_ in used_deps:
+                        toks.append(tok.lemma_)
+
+        # Fix noun_chunks and entities
+        text = " ".join(toks)
+        for entity in ents:
+            text = text.replace(entity, entity.replace(" ", "-"))
+
+        toks = text.split(" ")
 
         grammer_neighbors.append(toks)
-
-        # Get indicies for all words in the contexts list
-        # indicies = [i for i, j in enumerate(temp) if j in contexts]
 
         # Get surroundings of all context words
         for ind in range(0, len(toks)):
@@ -214,7 +151,7 @@ def HAL_context_grammar(texts, contexts, nlp, window_size=5):
                     data.append(value)
 
             # looking forward
-            for i in range(ind+1, ind+window_size+1):
+            for i in range(ind + 1, ind + window_size + 1):
                 if i < len(toks):
                     term = toks[i]
                     value = abs(i - window_size - ind - 1)
@@ -226,7 +163,11 @@ def HAL_context_grammar(texts, contexts, nlp, window_size=5):
                     data.append(value)
 
     # Create sparse matrices
-    csr_forward = csr_matrix((data, (row, col)), shape=(len(vocabulary), len(vocabulary)), dtype=float)
+    csr_forward = csr_matrix(
+        (data, (row, col)),
+        shape=(len(vocabulary), len(vocabulary)),
+        dtype=float)
+
     csr_backward = csr_forward.transpose()
 
     # Stack backward and forward matrices
@@ -238,7 +179,7 @@ def HAL_context_grammar(texts, contexts, nlp, window_size=5):
     hal = csr_forward + csr_backward
 
     # Normalize to [0,1]
-    max_value = 1/hal.max()
+    max_value = 1 / hal.max()
     hal = hal.multiply(max_value)
 
     return hal, vocabulary, grammer_neighbors
@@ -246,6 +187,7 @@ def HAL_context_grammar(texts, contexts, nlp, window_size=5):
 
 class Embedding():
     """Create word embeddings."""
+
     def __init__(self, lang, model, articles):
         '''
             lang: Spacy language string (en, de, ...)
