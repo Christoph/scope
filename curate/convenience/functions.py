ec23a561e3099f2ecd13ef6e5fc4ba949ddd50ee
Paul Boes <paulboes@Pauls-MBP.fritz.box>
2017-05-14 13:46:07 +0200

    added export_csv function(stem) and new mail template

--- curate/convenience/functions.py
+++ curate/convenience/functions.py
@@ -1,7 +1,8 @@
 from curate.models import Curate_Customer, Curate_Query, Article_Curate_Query
 from scope.models import Customer
-
-from datetime import date, timedelta
+import csv
+from django.utils.encoding import smart_str
+#from datetime import date, timedelta
 
 def retrieve_objects(customer_key, range=None):
 	customer = Customer.objects.get(customer_key=customer_key)
@@ -12,3 +13,21 @@ def retrieve_objects(customer_key, range=None):
 		articles = Article_Curate_Query.objects.filter(curate_query__in=queries).all()
 		return customer, curate_customer, queries, articles
 	return customer, curate_customer
+
+
+def export_csv(queryset):
+	file = open('queryset2string.csv', 'w+')
+	writer = csv.writer(file, csv.excel)
+	#produce titles
+	writer.writerow([
+		smart_str(u"ID"),
+		smart_str(u"Title"),
+		smart_str(u"Description"),
+	])
+	#write datat
+	for obj in queryset:
+		writer.writerow([
+			smart_str(obj.pk),
+			smart_str(obj.title),
+			smart_str(obj.description),
+		])
