eda17429f55122219e6591e22b965fb2446d4aff
Chris <christoph.kralj@gmail.com>
2017-06-06 13:00:33 +0200

    Added word vector embedding function for testing purposes

--- scope/methods/semantics/document_embedding.py
+++ scope/methods/semantics/document_embedding.py
@@ -23,6 +23,8 @@ class Embedding():
 
         if model == "grammar_svd":
             self._create_svd_embeddings(articles)
+        if model == "grammar_wv":
+            self._create_wv_embeddings(articles)
         else:
             raise Exception("Model not known.")
 
@@ -39,12 +41,35 @@ class Embedding():
 
         self._create_svd_embedding(clean_text)
 
+    def _create_wv_embeddings(self, articles):
+        '''
+            Creates an SVD embedding.
+
+            articles: List of Article objects
+
+            return: Embedding vectors
+        '''
+
+        clean_text = self.preprocessor.noun_based_preprocessing(articles)
+
+        self._create_wv_embedding(clean_text)
+
     def _create_svd_embedding(self, clean_text):
         vectorizer = TfidfVectorizer(sublinear_tf=True)
         tfidf = vectorizer.fit_transform(clean_text)
 
         # similarities
         self.vecs = TruncatedSVD(n_components=18).fit_transform(tfidf)
+        self.sim = cosine_similarity(self.vecs)
+
+    def _create_wv_embedding(self, clean_text):
+        docs = [self.nlp(text) for text in clean_text]
+        vecs = []
+        for doc in docs:
+            vecs.append(doc.vector)
+
+        # similarities
+        self.vecs = vecs
         self.sim = cosine_similarity(self.vecs)
 
     def get_similarity_matrix(self):
