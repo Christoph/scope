20aae7fa09ec2363e1be5b7740595c8ad8a80164
root <root@DESKTOP-J23JEOC.localdomain>
2017-08-18 13:50:38 +0200

    Added missing packages.

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django_extensions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'explore',
